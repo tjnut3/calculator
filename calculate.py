e1526972fdb586c723b85bd121bf827a5170b2ea
tjnut3 <notakcoblet@gmail.com>
2024-09-12 11:50:28 +0700

    operand

--- calculate.py
+++ calculate.py
@@ -14,17 +14,27 @@ def apply_operator(operands, operator):
     elif operator == '/':
         operands.append(a / b)
 
+# ฟังก์ชันการจัดการกับสมการ และลำดับความสำคัญ
 def calculate(expression):
     operand_stack = []
     operator_stack = []
     
     i = 0
     while i < len(expression):
+        # Str เป็นตัวเลขไหม or (เป็นจุดไหม and (ไม่ใช่ Str ตัวสุดท้าย and ตัวต่อไปเป็นตัวเลข))
+        # if true จะถูกนำไปเก็บเป็น str 
         if expression[i].isdigit() or (expression[i] == '.' and (i+1 < len(expression) and expression[i+1].isdigit())):
             num_str = ''
+            # Str ไม่ใช่ตัวสุดท้าย and (str เป็นตัวเลข or str คือ .)
+            # while true เก็บค่าไว้ใน num_str
             while i < len(expression) and (expression[i].isdigit() or expression[i] == '.'):
                 num_str += expression[i]
+                print(f"str = ", len(expression))
+                print(f"i = ", i)
+                print(num_str)
+                # เช็คตัวต่อไปเลยว่าตัวต่อไปเป็นตัวเลข
                 i += 1
+            # แปลง str ที่เก็บใน num_str ทั้งหมดไปเป็น float และนำเข้า list operand_stack
             operand_stack.append(float(num_str))
             i -= 1
         elif expression[i] == '(':
@@ -41,5 +51,7 @@ def calculate(expression):
     
     while operator_stack:
         apply_operator(operand_stack, operator_stack.pop())
-
     return operand_stack[-1]
+
+print(calculate("2+0.3"))
+print(len("2+0.3"))
