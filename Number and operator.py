aa5033c5473b8b955b4a846370ec383ca0d94513
tjnut3 <notakcoblet@gmail.com>
2024-09-30 20:57:02 +0700

    merged

--- Number and operator.py
+++ Number and operator.py
@@ -8,19 +8,15 @@ def calculator_input():
         # รับ input จากผู้ใช้
         user_input = input("ป้อนตัวเลข (0-9) หรือตัวดำเนินการ (+, -, *, /, (, )): ")
 
-        # ตรวจสอบหากผู้ใช้ต้องการออกจากโปรแกรม
-        if user_input.lower() == 'exit':
-            print("ออกจากโปรแกรมแล้ว ขอบคุณที่ใช้บริการ!")
-            break
-
         # ตรวจสอบว่าข้อมูลที่ป้อนถูกต้องหรือไม่ (ตัวเลข 0-9 หรือ +, -, *, /, (, ))
-        if user_input in '0123456789+-*/()=':
+        if user_input in '0123456789+-*/()':
             # บันทึกค่า
             inputs.append(user_input)
+            print(inputs)
             print(f"ค่าเก็บอยู่: {' '.join(inputs)}")
+        elif user_input in '=':
+            return ' '.join(inputs)
         else:
             print("ค่าที่ป้อนไม่ถูกต้อง กรุณาป้อนเฉพาะตัวเลข 0-9 หรือตัวดำเนินการ (+, -, *, /, (, ))")
-print("Again")
-print("ลองเพิ่มดูอีกรอบ")
-calculator_input()
-#TT
+
+input = calculator_input()
